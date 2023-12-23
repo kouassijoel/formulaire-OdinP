d49cddf3031d86910df0e95332889bf3cb2967f1
kouassi joel <kouassijoel038@gmail.com>
2023-12-23 07:17:08 +0000

    Created project regisform"

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
-function validate() {
- 
-    var a = document.getElementById("mdp").value;
-    var b = document.getElementById("confirm_mdp").value;
-
-    if (a!=b) {
-        alert("Les mots de passe ne correspondent pas.");
-        return false; }
-    else {
-        alert("Les mots de passe correspondent.");
-        return false; }
+let passConfirm = function () {
+    if (document.getElementById("password").value ==
+        document.getElementById("ConfirmPassword").value) {
+        document.getElementById("message").style.color = "green";
+        document.getElementById("message").innerHTML = "Passwords match!"
+    } else {
+        document.getElementById("message").style.color = "red";
+        document.getElementById("message").innerHTML = "*Passwords do NOT match!"
+    }
 }
+
+passConfirm()
